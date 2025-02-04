773856890abd6f7e5c117f791e7337d5a99a400d
mp <mp@Ubuntu.myguest.virtualbox.org>
2025-02-04 14:15:38 +0100

    pylint

--- compiler.py
+++ compiler.py
@@ -4,7 +4,7 @@ import os
 def get_labels():
     labels = []
 
-    for line in open("labels.txt",'r').readlines():
+    for line in open("labels.txt",'r',encoding="UTF-8").readlines():
         split_line = line.split(" ")
         if split_line[0] == "HALT":
             mx = int(split_line[1])
@@ -27,7 +27,7 @@ def set_labels(labels,mx,source_file,result_file):
                 line = source_file[i].strip()+" "+str(num)+"\n"
             else:
                 line = source_file[i].strip()+" "+label
-            if i!=0 or line != "JUMP 1\n": 
+            if i!=0 or line != "JUMP 1\n":
                 result_file.write(line)
             labels.pop(0)
         else:
@@ -36,10 +36,10 @@ def set_labels(labels,mx,source_file,result_file):
 def check_errors(output):
     if output == "done\n":
         return
-    else:
-        print(output,end="")
-        os.system("rm -f temp1.mr temp2.mr labels.txt")
-        sys.exit(0)
+
+    print(output,end="")
+    os.system("rm -f temp1.mr temp2.mr labels.txt")
+    sys.exit(0)
 
 parser_output_1 = os.popen("./compiler "+sys.argv[1]).read()
 check_errors(parser_output_1)
@@ -47,8 +47,8 @@ check_errors(parser_output_1)
 parser_output_2 = os.popen("./labels").read()
 check_errors(parser_output_2)
 
-source_file = open("temp2.mr",'r').readlines()
-result_file = open(sys.argv[2],'w')
+source_file = open("temp2.mr",'r',encoding="UTF-8").readlines()
+result_file = open(sys.argv[2],'w',encoding="UTF-8")
 
 labels,mx = get_labels()
 set_labels(labels,mx,source_file,result_file)
